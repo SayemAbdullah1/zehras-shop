62e47e7d810b0a01427bfdbc2023ff1ca82b4ab6
SayemAbdullah1 <dxsayem0@gmail.com>
2023-03-25 14:34:25 +0600

    add product model in seed

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,4 @@
+import Product from "@/models/Products";
 import User from "@/models/User";
 import data from "@/utilities/data";
 import db from "@/utilities/db";
@@ -8,6 +9,8 @@ const handler = async (req, res) =>{
     await db.connect();
     await User.deleteMany();
     await User.insertMany(data.users)
+    await Product.deleteMany();
+    await Product.insertMany(data.products)
     await db.disconnect();
     res.send({message: 'seeded successfully'})
 }
